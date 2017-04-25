ed032b521662ba41fa7e69b07a1e67034234f7b9
Jan Piskvor Martinec <github@piskvor.org>
2017-04-26 01:37:46 +0200

    more import/export tests

--- tests/GolAppTest.php
+++ tests/GolAppTest.php
@@ -5,6 +5,7 @@ namespace Piskvor\Test;
 
 use PHPUnit\Framework\TestCase;
 use Piskvor\GolApp;
+use Piskvor\LifeBoard;
 
 class GolAppTest extends TestCase
 {
@@ -17,7 +18,7 @@ class GolAppTest extends TestCase
 
     public function testImportInvalidXml()
     {
-        $ga = new GolApp(__DIR__ .'/invalid-xml.xml');
+        $ga = new GolApp(__DIR__ . '/invalid-xml.xml');
         $this->expectException(\Exception::class);
         // do you even XML
         $ga->parseFile();
@@ -25,7 +26,7 @@ class GolAppTest extends TestCase
 
     public function testImportNoWorld()
     {
-        $ga = new GolApp(__DIR__ .'/no-world.xml');
+        $ga = new GolApp(__DIR__ . '/no-world.xml');
         $ga->parseFile();
         // /life/world is missing from XML
         $this->assertFalse($ga->getBoard()->isInitialized());
@@ -33,13 +34,61 @@ class GolAppTest extends TestCase
 
     public function testImportExtraneous()
     {
-        $ga = new GolApp(__DIR__ .'/extraneous.xml');
+        $ga = new GolApp(__DIR__ . '/extraneous.xml');
         $ga->parseFile();
         $lb = $ga->getBoard();
         // extra XML elements are ignored, the board is initialized
         $this->assertTrue($lb->isInitialized());
         // there's nothing on the board
-        $this->assertCount(0,$lb->getAllOrganisms());
+        $this->assertCount(0, $lb->getAllOrganisms());
+    }
+
+    public function testImportExport()
+    {
+        $infile = __DIR__ . '/small-world.xml';
+        $outfile = __DIR__ . '/../out.xml';
+        $ga = new GolApp($infile, $outfile);
+        $ga->parseFile();
+        $ga->exportFile($ga->getBoard());
+
+        // see if imported and exported version matches
+        $expected = new \DOMDocument();
+        $expected->load($infile);
+        $actual = new \DOMDocument();
+        $actual->load($outfile);
+        $this->assertEqualXMLStructure($expected->documentElement, $actual->documentElement);
+    }
+
+    public function testOutsideSourceExport()
+    {
+        $compareFile = __DIR__ . '/small-world.xml';
+        $outfile = __DIR__ . '/../out.xml';
+        // we are NOT parsing the file
+        $ga = new GolApp($compareFile, $outfile);
+        // match its structure in code instead
+        $lb = new LifeBoard();
+        $lb->setEdgeSize(20)
+            ->setSpeciesCount(3)
+            ->setMaxIterations(300);
+        foreach ([10, 11] as $x) {
+            foreach ([0, 1] as $y) {
+                $lb->importOrganism($x, $y, 'a');
+            }
+        }
+        $lb->importOrganism(2, 1, 't');
+        $lb->importOrganism(3, 2, 't');
+        foreach ([1, 2, 3] as $x) {
+            $lb->importOrganism($x, 3, 't');
+        }
+
+        $ga->exportFile($lb);
+
+        // see if coded and exported version matches
+        $expected = new \DOMDocument();
+        $expected->load($compareFile);
+        $actual = new \DOMDocument();
+        $actual->load($outfile);
+        $this->assertEqualXMLStructure($expected->documentElement, $actual->documentElement);
     }
 
 }
